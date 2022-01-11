b0eee794d8405290e11d70dd2cdd4c6770761c79
xeemv <xeeyang08@gmail.com>
2022-01-11 17:13:16 -0600

    update

--- pages/smtp.js
+++ pages/smtp.js
@@ -1,9 +1,9 @@
 function sendEmail(){
     Email.send({
-        // SecureToken: "67e3554c-e7ee-4d9d-93d0-8611ed03bdf3",
-        Host : "smtp.gmail.com",
-        Username : "xeeyang08@gmail.com",
-        Password : "ooedqzmqatcrcold",
+        SecureToken: "1f84f0e3-6e59-4146-b861-d5e6fd92f50c",
+        // Host : "smtp.gmail.com",
+        // Username : "xeeyang08@gmail.com",
+        // Password : "ooedqzmqatcrcold",
         To : 'xeeyang08@gmail.com',
         From : document.getElementById("email").value,
         Subject : "New Contact Form Inquiry",
