c31436e5d0c9c3e6b3feae56d8044b3c5c1ad8e3
xeemv <xeeyang08@gmail.com>
2022-01-11 10:56:13 -0600

    update

--- pages/smtp.js
+++ pages/smtp.js
@@ -6,7 +6,7 @@ function sendEmail(){
         To : 'xeeyang08@gmail.com',
         From : document.getElementById("email").value,
         Subject : "New Contact Form Inquiry",
-        Body : "Name: " + document.getElementById("Name").value + "<br> Email: " + document.getElementById("email").value
+        Body : "Name: " + document.getElementById("name").value + "<br> Email: " + document.getElementById("email").value
         + "<br> Phone number: " + document.getElementById("phone").value
         + "<br> Message: " + document.getElementById("message").value
     }).then(
